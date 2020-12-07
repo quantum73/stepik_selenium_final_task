705da1e3ed5b6cc47587a9b34a6284fbb1d68c79
quantum <fominquant@yandex.ru>
2020-12-07 14:18:22 +0300

    Updated solution for add in basket on product page task

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 from selenium.common.exceptions import NoAlertPresentException
 
@@ -12,10 +13,28 @@ class ProductPage(BasePage):
         assert self.is_element_present(
             *ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
 
+    def should_be_success_messages(self):
+        assert self.is_element_present(
+            *ProductPageLocators.SUCCESS_MSG_WITH_PRODUCT_NAME), "No success messages with product name"
+        assert self.is_element_present(
+            *ProductPageLocators.SUCCESS_MSG_WITH_PRODUCT_PRICE), "No success messages with product price"
+
+    def should_be_product_price_equals_value_of_basket(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.strip()
+        message_with_price = self.browser.find_element(*ProductPageLocators.SUCCESS_MSG_WITH_PRODUCT_PRICE).text.strip()
+        product_price = float(re.findall(r'\d+\.\d+', product_price)[0])
+        price_in_message = float(re.findall(r'\d+\.\d+', message_with_price)[0])
+        assert product_price == price_in_message, "Product price corresponds to the value of the basket"
+
+    def should_be_product_name_matches_product_name_in_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text.strip()
+        message_with_price = self.browser.find_element(*ProductPageLocators.SUCCESS_MSG_WITH_PRODUCT_NAME).text.strip()
+        assert product_name in message_with_price, "Product name matches the product name that was added to the basket"
+        print("--- Product add to basket! ---")
+
     def add_product_to_basket(self):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_btn.click()
-        print("--- Product add to basket! ---")
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,31 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
-    product_page.open()  # открываем страницу
-    product_page.should_be_add_to_basket_button()  # проверяем, есть ли на странице кнопка добавления в корзину
-    product_page.add_product_to_basket()  # кликаем на кнопку добавления в корзину
-    product_page.solve_quiz_and_get_code()  # обрабатываем alert и вычисляем результат
+    product_page.open()
+    # проверяем, есть ли на странице кнопка добавления в корзину
+    product_page.should_be_add_to_basket_button()
+    # кликаем на кнопку добавления в корзину
+    product_page.add_product_to_basket()
+    # обрабатываем alert и вычисляем результат
+    product_page.solve_quiz_and_get_code()
+    # проверяем появились ли сообщения о добавлении товара к корзину
+    product_page.should_be_success_messages()
+    # проверяем что название товара в сообщении совпадает с названием товара, который действительно был добавлен
+    product_page.should_be_product_name_matches_product_name_in_basket()
+    # проверяем что стоимость корзины совпадает с ценой товара
+    product_page.should_be_product_price_equals_value_of_basket()
