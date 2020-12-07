e316dd6f1c5734bcac2349f330ff118c312592d6
quantum <fominquant@yandex.ru>
2020-12-07 12:58:53 +0300

    Minor fix

--- test_main_page.py
+++ test_main_page.py
@@ -3,13 +3,9 @@ from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    # page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    # page.open()  # открываем страницу
-    # page.should_be_login_link()  # проверяем наличие ссылки на страницу логина
-    # page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()
-    page.should_be_login_page()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()  # делаем проверки ссылки, формы логина и формы регистрации на новой странице
